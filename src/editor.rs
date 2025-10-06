1cc7a9f3ff6bb2a1abb1788f681aa481e68bfcc6
MCdyc <2426000156@qq.com>
2025-10-06 19:22:10 +0800

    封装终端操作，实现打印~

--- src/editor.rs
+++ src/editor.rs
@@ -1,55 +1,77 @@
-//导入外部库
 use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers, read};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-
+mod terminal;
+//导入外部库
+use std::io::Error;
+use terminal::{Position, Size, Terminal};
 pub struct Editor {
     should_quit: bool,
 }
 
 impl Editor {
     //初始化编辑器
-    pub fn default() -> Self {
+    //常量函数
+    pub const fn default() -> Self {
         Editor { should_quit: false }
     }
     //运行编辑器
     pub fn run(&mut self) {
-        //调用并匹配错误
-        if let Err(err) = self.repl() {
-            //错误处理
-            panic!("{err:#?}");
+        //使用unwarp 将Error转换为panic
+        Terminal::initialize().unwrap();
+        let result = self.repl();
+        Terminal::terminate().unwrap();
+        result.unwrap();
+    }
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let Size { width: _, height } = Terminal::size()?;
+
+        for current_rows in 0..height {
+            Terminal::clear_line()?;
+            Terminal::print("~")?;
+            if current_rows < height - 1 {
+                Terminal::print("\r\n")?;
+            }
         }
-        print!("Goodbye.\r\n");
+        Ok(())
+    }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        Terminal::hide_cursor()?;
+        if self.should_quit {
+            Terminal::clear_screen()?;
+            Terminal::print("Goodbye\r\n")?;
+        } else {
+            Self::draw_rows()?;
+            Terminal::move_cursor_to(Position { x: 0, y: 0 })?;
+        }
+        Terminal::show_cursor()?;
+        Terminal::execute()?;
+        Ok(())
+    }
+
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Event::Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                //使用.contains()代替指针解引用
+                KeyCode::Char('d') if modifiers.contains(KeyModifiers::CONTROL) => {
+                    self.should_quit = true;
+                }
+                _ => {}
+            }
+        }
     }
     //  运行编辑器的 REPL 循环，返回 Result
     fn repl(&mut self) -> Result<(), std::io::Error> {
         //简化错误处理
-        enable_raw_mode()?;
         loop {
-            //读取事件,获取当前按键
-            //code为按键值，modifiers为修饰符，kind为按键类型，state为按键状态
-            if let Event::Key(KeyEvent {
-                code,
-                modifiers,
-                kind,
-                state,
-            }) = read()?
-            {
-                println!(
-                    "Code: {code:?} Modifiers: {modifiers:?} Kind: {kind:?} State: {state:?} \r"
-                );
-                match code {
-                    KeyCode::Char('q') if modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-                    }
-                    _ => (),
-                }
-            }
-            //每次循环检查是否退出
+            self.refresh_screen()?;
             if self.should_quit {
                 break;
             }
+            let enent = read()?;
+            self.evaluate_event(&enent);
         }
-        disable_raw_mode()?;
         Ok(())
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 #![warn(clippy::all, clippy::pedantic)]
 //导入当前文件夹中存在的模块
 mod editor;
+
 //使用模块中的结构体
 use editor::Editor;
 
